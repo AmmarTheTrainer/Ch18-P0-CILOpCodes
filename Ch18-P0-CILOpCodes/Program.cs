d721e2965edf139a318f2a32796d5defed1f0f3a
Ammar The Trainer <ammarthetrainer@gmail.com>
2018-11-26 16:18:35 +0500

    Project Created

---
+++ Ch18-P0-CILOpCodes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch18_P0_CILOpCodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
